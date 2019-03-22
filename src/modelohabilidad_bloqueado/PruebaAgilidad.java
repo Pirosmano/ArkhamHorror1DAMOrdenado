eab7d47ec71dfd849c90ceb0dbf9253ca1fbcfe7
miguel <miguel@DAM5>
2019-03-22 12:58:52 +0100

    1

--- src/modelohabilidad_bloqueado/PruebaAgilidad.java
+++ src/modelohabilidad_bloqueado/PruebaAgilidad.java
@@ -7,7 +7,6 @@ package modelohabilidad_bloqueado;
 
 import Modelo.BolsaDelCaos.BolsaDelCaos;
 import Modelo.Cartas.Carta;
-import Modelo.Cartas.BDCarta;
 import Modelo.RolandBanks;
 import java.util.ArrayList;
 import java.util.LinkedList;
