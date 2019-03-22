93ccb6e01b1ac3b1a847c1aa228a25425a7f315d
miguel <miguel@DAM5>
2019-03-22 12:53:48 +0100

    1

--- src/Modelo/RolandBanks.java
+++ src/Modelo/RolandBanks.java
@@ -36,10 +36,7 @@ public class RolandBanks extends Investigador {
        
     }
 
-    @Override
-    public void moverPersonaje(ArrayList<Lugar> lugares, Personaje personaje, Lugares l) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+    
 
  
   
